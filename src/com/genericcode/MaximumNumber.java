82908436917f1676c95cc0fb663a1349bec28222
T RAVI KUMAR <91825694+ravikumar990@users.noreply.github.com>
2021-11-12 14:29:35 +0530

    Update MaximumNumber.java

--- src/com/genericcode/MaximumNumber.java
+++ src/com/genericcode/MaximumNumber.java
@@ -3,23 +3,23 @@ package com.genericcode;
 public class MaximumNumber {
  
 	public static void main(String[] args) {
-		// Three Float objects initialization
-		Float firstNumber = 54.34f;
-		Float secondNumber = 34.33f;
-		Float thirdNumber = 54.65f;
+		// Three Integer objects initialization
+		Integer firstNumber = 54 ;
+		Integer secondNumber = 34 ;
+		Integer thirdNumber = 21 ;
 
-		Float max = firstNumber;
+		Integer max = firstNumber ;
 
 		// using compareTo method
-		if (secondNumber.compareTo(max) > 0) {
+		if(secondNumber.compareTo(max) > 0) {
 			max = secondNumber;
 		}
-		if (thirdNumber.compareTo(max) > 0) {
-			max = thirdNumber;
+		if(thirdNumber.compareTo(max) > 0) {
+			max = thirdNumber ;
 		}
 
-		// printing the maximum among three Float objects
-		System.out.println("Maximum number is " + max);
+		// printing the maximum among three integer objects
+		System.out.println("Maximum number is "+max);
 	}
 
 }
