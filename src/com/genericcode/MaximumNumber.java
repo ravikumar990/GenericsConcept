554e06c1f9b29e39700ea9e1e1a0b5409c8918b5
ravi <ravi@gmail.com>
2021-11-12 12:32:16 +0530

    Added : Code for find max among three integers

---
+++ src/com/genericcode/MaximumNumber.java
@@ -0,0 +1,25 @@
+package com.genericcode;
+
+public class MaximumNumber {
+
+	public static void main(String[] args) {
+		// Three Integer objects initialization
+		Integer firstNumber = 54;
+		Integer secondNumber = 34;
+		Integer thirdNumber = 21;
+
+		Integer max = firstNumber;
+
+		// using compareTo method
+		if (secondNumber.compareTo(max) > 0) {
+			max = secondNumber;
+		}
+		if (thirdNumber.compareTo(max) > 0) {
+			max = thirdNumber;
+		}
+
+		// printing the maximum among three integer objects
+		System.out.println("Maximum number is " + max);
+	}
+
+}
