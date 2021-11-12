16476db8ae55fab85ceef4bec1221d37092875e9
ravi <ravi@gmail.com>
2021-11-12 14:38:27 +0530

    Added : Code to find max among three Strings

--- src/com/genericcode/MaximumNumber.java
+++ src/com/genericcode/MaximumNumber.java
@@ -1,14 +1,14 @@
 package com.genericcode;
 
 public class MaximumNumber {
- 
-	public static void main(String[] args) {
-		// Three Float objects initialization
-		Float firstNumber = 54.34f;
-		Float secondNumber = 34.33f;
-		Float thirdNumber = 54.65f;
 
-		Float max = firstNumber;
+	public static void main(String[] args) {
+		// Three String objects initialization
+		String firstNumber = "ram";
+		String secondNumber = "ramesh";
+		String thirdNumber = "rama";
+
+		String max = firstNumber;
 
 		// using compareTo method
 		if (secondNumber.compareTo(max) > 0) {
@@ -18,8 +18,8 @@ public class MaximumNumber {
 			max = thirdNumber;
 		}
 
-		// printing the maximum among three Float objects
-		System.out.println("Maximum number is " + max);
+		// printing the maximum among three String objects
+		System.out.println("Maximum string is " + max);
 	}
 
 }
